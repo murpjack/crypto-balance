a14096585773764128144e7370bb556685de075f
Jack <jackmurphy756@gmail.com>
2019-04-29 08:58:24 +0100

    refactored getRatesData fn using a map, removed async/await

--- myscript.js
+++ myscript.js
@@ -1,32 +1,14 @@
-
-let selectedRates = [{
-    id: "BAT",
-    name: "Basic Attention Token"
-  },
-  {
-    id: "BTC",
-    name: "Bitcoin"
-  },
-  {
-    id: "ETC",
-    name: "Etherium Classic"
-  },
-  {
-    id: "ETH",
-    name: "Etherium"
-  },
-  {
-    id: "LTC",
-    name: "Litecoin"
-  },
-  {
-    id: "XRP",
-    name: "Ripple"
-  }
+let selectedRates = [
+  "BAT",
+  "BTC",
+  "ETC",
+  "ETH",
+  "LTC",
+  "XRP"
 ];
 
 
-function toLowerCase(abr) {
+function returnImgName(abr) {
   switch (abr) {
     case "BTC":
       return "btc"
@@ -52,7 +34,33 @@ function toLowerCase(abr) {
   }
 }
 
-function setCurrencySymbol(curr) {
+function returnFullName(abr) {
+  switch (abr) {
+    case "BTC":
+      return "Bitcoin"
+      break;
+    case "BAT":
+      return "Basic Attention Token"
+      break;
+    case "ETC":
+      return "Etherium Classic"
+      break;
+    case "ETH":
+      return "Etherium"
+      break;
+    case "LTC":
+      return "Litecoin"
+      break;
+    case "XRP":
+      return "Ripple"
+      break;
+    default:
+      return "Bitcoin"
+      break;
+  }
+}
+
+function returnCurrencySymbol(curr) {
   switch (curr) {
     case "BTC":
       return "Ƀ"
@@ -74,91 +82,54 @@ function setCurrencySymbol(curr) {
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  getRatesData();
+  getRatesData()
 });
 
-const BASE_CURRENCY = "GBP";
-function url(id) {
-  return `https://api.coinbase.com/v2/prices/${id}-${BASE_CURRENCY}/spot`
-};
-// Improve naming conventions!!! *** To do ***
-// loop through fetches
 function getRatesData() {
-  // loop through fetch synchronously,
-  // on last fetch resolve promise && build HTML
-  function fetchData(rate, index, array) {
-    console.log(rate.id);
-    fetch(url(rate.id))
-      .then(response => {
-        console.log(rate.id);
-        return response.json();
-      })
-      .then(json => {
-        // Add data to selectedRates obj
-        console.log(21,rate);
-        console.log("data to selectedRates", json.data);
-        return returnRateData(json.data, rate, array);
-      })
-      .then(array => {
-        console.log("array", array);
-        return array;
-        // returnRateData(json.data);
-      })
-      .catch(function(err) {
-        console.log('Fetch Error :-S', err);
-      });
-    //   // addDataToSelectedRates(value, rate, index, array);
-  }
+  selectedRates.map((rate) => {
+    fetchData(rate);
+    console.log(fetchData(rate));
+    // Promise.all([])
+  });
+}
 
-  // forEach doesn't wait for fetch.thens so I am using async/await
-  async function asyncForEach(array, callback) {
-    for (let index = 0; index < array.length; index++) {
-      await callback(array[index], index, array);
-    } // END for loop
-  } // END asyncForEach fn
-
-
-  const start = async () => {
-    await asyncForEach(selectedRates, async (rate, index, array) => {
-      await fetchData(rate, index, array);
+function fetchData(rate) {
+  let spotUrl = `https://api.coinbase.com/v2/prices/${rate}-GBP/spot`;
+  return fetch(spotUrl)
+    .then(response => response.json())
+    .then(json => (json) => createArticle(json.data))
+    .catch(function(err) {
+      console.log('Fetch Error :-S', err);
     });
-    console.log("Callback", fetchData());
-    // appendRatesListToBody(selectedRates);
-  };
-  start();
 }
 
-function returnRateAmount(data) {
-  console.log(230);
-  let currencySymbol = setCurrencySymbol(data.currency);
-  // convert string into a number, then to 2dp
-  let value = parseFloat(data.amount).toFixed(2);
-  let valueString = currencySymbol + value;
-  return {
-    "currentValue": valueString
-  };
+function createArticle(data) {
+  let base = data.base;
+  let article = document.createElement("article");
+  article.setAttribute("id", base);
+  article.classList.add('rate');
+
+  let currencySymbol = returnCurrencySymbol(data.currency);
+  let roundUpValue = parseFloat(data.amount).toFixed(2);
+  let valueString = currencySymbol + roundUpValue;
+
+  let bat = "";
+  base === "BAT" ? bat = "name--bat" : bat = "";
+  article.innerHTML =
+    `<img src="./node_modules/cryptocurrency-icons/32/color/${returnImgName(base)}.png" class="rate__image"/>
+     <div class='rate__name name ${bat}'>
+       <h1 class='name--full'><strong>${returnFullName(base)}</strong></h1>
+       <h2 class='name--short'>${base}</h2>
+     </div>
+     <span class='rate__value value'>${valueString}
+     </span>`;
+  return article;
 }
 
-function returnRateIcon(data) {
-  console.log(231);
-  return {
-    "icon": `./node_modules/cryptocurrency-icons/32/color/${toLowerCase(data.id)}.png`
-  };
-}
-
-function addValueToRate(namedValue, rate) {
-  console.log(232);
-  let newRate = {
-    ...rate,
-    ...namedValue
-  };
-}
-
-function returnRateData(data, rate, array) {
-  console.log(22, );
-  addValueToRate(returnRateIcon(data), rate);
-  addValueToRate(returnRateAmount(data), rate);
-  return array;
+function buildRatesList(data) {
+  let ratesContent = [];
+  data.map(rate => createArticle(rate, ratesContent));
+  return ratesContent;
 }
 
 function appendRatesListToBody(data) {
@@ -168,33 +139,6 @@ function appendRatesListToBody(data) {
   let ratesList = buildRatesList(data);
   ratesList.map(item => rates.appendChild(item));
 }
-
-function buildRatesList(data) {
-  let ratesContent = [];
-
-  function createArticle(rate, list) {
-    console.log(33, rate);
-    let article = document.createElement("article");
-    let bat = "";
-    article.setAttribute("id", rate.id);
-    article.classList.add('rate');
-    if (rate.id === "BAT") {
-      bat = "name--bat";
-    }
-    article.innerHTML =
-      `<img src="${rate.icon}" class="rate__image"/>
-       <div class='rate__name name ${bat}'>
-         <h1 class='name--full'><strong>${rate.name}</strong></h1>
-         <h2 class='name--short'>${rate.id}</h2>
-       </div>
-       <span class='rate__value value'>${rate.currentValue}
-       </span>`;
-    return list.push(article);
-  }
-  data.map(rate => createArticle(rate, ratesContent));
-  return ratesContent;
-}
-
 
 
 function refreshRates(e) {
