520da9c325e1731cd62a0ed695d85d7308e7086c
jack.murphy <jack.murphy@gemporia.com>
2019-11-29 12:54:41 +0000

    add tests of default state and fetchRates api call

--- test/app.test.js
+++ test/app.test.js
@@ -1,17 +1,39 @@
 const fetchMock = require("fetch-mock")
 const fetch = require("node-fetch")
 const Future = require('fluture')
-const response = {
-  hello: "world"
-};
 
 describe("fetchRate", () => {
-  it("state updates on successful fetch", () => {
+
+  it("Default state set to NotAsked", () => {
+    const selectedRates = ["BTC", "ETC", "ETH", "LTC", "XRP"];
+    const setDefaultState = r => ({
+      [r]: {
+        status: "NotAsked"
+      }
+    });
+    const defaultState = selectedRates.map(setDefaultState);
+    defaultState.map((s, idx) => expect(s[selectedRates[idx]].status).toEqual("NotAsked"))
+  });
+
+  it("FetchRate item", () => {
     const url = "https://api.coinbase.com/v2/prices/BTC-GBP/spot";
+    const response = {
+      "data": {
+        "amount": "5853.81420561",
+        "base": "BTC",
+        "currency": "GBP",
+      }
+    };
+
     fetchMock.mock(url, 200)
     fetch(url)
       .then(res => res.json())
-      .then(res => expect(res).toEqual(response))
+      .then(res => {
+        if (res.status >= 200 && res.status < 300) {
+          expect(res).toEqual(response)
+        }
+      })
       .catch(err => `Oops! ${err.message}`)
   });
+
 });
