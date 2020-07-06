afbf3b4f544457feeac272ea257e9792530f3f29
Jack Murphy <30591358+murpjack@users.noreply.github.com>
2020-07-06 21:29:56 +0100

    Update login.js

--- src/constants/login.js
+++ src/constants/login.js
@@ -1,8 +1,6 @@
-export const CLIENT_ID =
-  "ee13f194f5631432e54213d20da4d929ebc7dc8a2b0d644af69a1eb08081a0f0";
+export const CLIENT_ID = "";
 
-export const CLIENT_SECRET =
-  "2767ca0eb1ffa53f2791899cb6f3a816208e3b2f3cf9ec19b02ca04b314a9b6b";
+export const CLIENT_SECRET = "";
 
 export const SUCCESS_URI = "https://murphyme.co.uk/calypso/success";
 
